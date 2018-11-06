ab585527a3bb0b5a75dc5cac2c020a7a168ed861
leorodr501 <lrodr501@fiu.edu>
2018-11-05 19:17:58 -0500

    "done"

---
+++ madlibs.py
@@ -0,0 +1,236 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter the name of a Celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
+
+## Mad Lib 1
+
+date = input("Enter a date: ")
+full_name = input("Enter a full name: ")
+a_place = input("Enter the name of a place: ")
+class_noun = input("Enter a class subject: ")
+signature = input("Enter a signature: ")
+
+print("Date: " + date)
+print(full_name + " is authorized"),
+print("to be at  " + a_place)
+print("instead of " + class_noun),
+print("class.")
+print("SIGNED: " + signature)
+
+## Mad Lib 2
+
+
+noun = input("Enter a noun: ")
+object = input("Enter an object: ")
+food = input("Enter your least favorite food: ")
+bad_things = input("Enter the funniest thing that comes to mind: ")
+verb = input("Enter a verb: ")
+
+print("                  We Are The Champions") 
+print("I've taken my " + noun + " out.")
+print("And my " + object + " calls")
+print("You brought me " + food + " and " + bad_things + " and everything that comes with it")
+print("I " + verb + " you all.")
+
+## Mad Lib 3
+
+
+adjective = input("Enter an adjective: ")
+object = input("Enter an object: ")
+imaginary_friend = input("Enter the name of an imaginary friend: ")
+adjective = input("Enter another adjective: ")
+verb = input("Enter a verb: ")
+
+print("          Under The Bridge")
+print("Sometimes I feel like " + adjective )
+print("I don't have a " + object )
+print("Sometimes I feel like my only friend is " + imaginary_friend )
+print("The " + adjective + " city ")
+print("Lonely as I am, together we " + verb )
+
+## Mad Lib 4 
+
+adjective = input("Enter an adjective: ")
+adjective = input("Enter another adjective: ")
+noun = input("Enter a noun: ")
+noun = input("Enter another noun: ")
+plural_noun = input("Enter a plural noun: ")
+game = input("Enter a game: ")
+plural_noun = input("Enter another plural noun: ")
+verb = input("Enter a verb ending in 'ing': ")
+verb = input("Enter another verb ending in 'ing': ")
+plural_noun = input("Enter another plural noun: ")
+verb = input("Enter another verb ending in 'ing': ")
+noun = input("Enter another noun: ")
+plant = input("Enter a plant; ")
+body_part = input("Enter a part of the body: ")
+place = input("Enter a place: ")
+verb = input("Enter another verb ending in 'ing': ")
+adjective = input("Enter another adjective: ")
+number = input("Enter a number <100: ")
+plural_noun = input("Enter another plural noun: ")
+
+print("A vacation is when you take a trip to some " + adjective + " place"),
+print(" with you " + adjective + " family.")
+print("Usually you go to some place thatis near a/an " + noun ),
+print(" or up on a/an " + noun + ".")
+print("A good vacation place is one where you can ride " + plural_noun ),
+print(" or play " + game + " or go hunting for " + plural_noun + ".")
+print("I like to spend my time " + verb + " or " + verb + ".")
+print("When parents go on a vacation, they spend their time eating three " + plural_noun + " a day,")
+print(" and fathers play golf, and mothers sit around " + verb + ".")
+print("Last summer, my little brother fell in a/an " + noun + " and got poison " + plant  ),
+print(" all over his " + body_part + ".")
+print("My family is going to go to (the) " + place + ", and I will practice " + verb + ".")
+print("Parents need vacations more than kids because parents are always very " + adjective + " and because they have to work " + number + " hours every day all year making enough " + plural_noun + " to pay for the vacation.")
+
+## Mad Lib 5
+
+name = input("Enter your name: ")
+date = input("Enter a date: ")
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb in past tense: ")
+adverb = input("Enter an adverb: ")
+adjective = input("Enter another adjective: ")
+noun = input("Enter another noun: ")
+noun = input("Enter another noun: ")
+adjective = input("Enter another adjective: ")
+verb = input("Enter a verb: ")
+adverb = input("Enter another adverb: ")
+verb = input("Enter another verb in past tense: ")
+adjective = input("Enter another adjective: ")
+
+print("Name: " + name + "     Date: " + date )
+print("Today I went to the zoo. I saw a " + adjective + " " + noun + " jumping up and down in its tree.")
+print("He " + verb + " " + adverb + " through the large tunnel that led to its " + adjective + " " + noun + ".")
+print("I got some peanuts and passed them through the cage to a gigantic gray " + noun + " towering above my head.")
+print("Feeding the animals made me hungry. I went to get a " + adjective + " scoop of ice cream. It filled my stomach.")
+print("Afterwards I had to " + verb + " " + adverb + " to catch our bus.")
+print("When I got home I " + verb + " my mom for a " + adjective + " day at the zoo.")
+
+## Mad Lib 6
+
+adjective = input("Enter an adjective: ")
+adjective = input("Enter another adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb in singular present form: ")
+fruit = input("Enter a fruit: ")
+noun = input("Enter another noun: ")
+fruit = input("Enter another fruit: ")
+object = input("Enter an object: ")
+animal = input("Enter an animal: ")
+adjective = input("Enter another adjective: ")
+plural_noun = input("Enter a plural noun: ")
+
+print("              The Rose Family")
+print("The rose is a " + adjective + ",")
+print("And was always a " + adjective + ".")
+print("But the " + noun + " now " + verb )
+print("That the " + fruit + " is a " + noun + ",")
+print("And the " + fruit + " is, and so is ")
+print("The " + object + ", I suppose.")
+print("The " + animal + " only knows")
+print("What will next prove a rose " + adjective + ".")
+print(plural_noun + " , of course, are a rose -")
+print("But were always a rose.")
+
+## Mad Lib 7
+
+name = input("Enter a name: ")
+date = input("Enter a date: ")
+plural_noun = input("Enter a plural noun: ")
+plural_noun = input("Enter another plural noun: ")
+verb = input("Enter a verb: ")
+celebrity = ("Enter the name of a celebrity: ")
+noun = input("Enter a noun: ")
+ing = input("Enter a verb ending in 'ing': ")
+plural_noun = input("Enter another plural noun: ")
+noun = input("Enter another noun: ")
+plural_noun = input("Enter another plural noun: ")
+
+print("Name: " + name)
+print("Date: " + date)
+print("          At The Arcade!")
+print("When I go to the arcade with my " + plural_noun + " there are lots of games to play.")
+print("I spend lots of time there with my friends. In 'Xmen' you can be different " + plural_noun + ".")
+print("The point of the game is to " + verb + " every robot.")
+print("You also need to save people, and then you can go to the next level. In 'Star Wars' you are " + celebrity + " and you try to destroy every " + noun + ".")
+print("In a car racing / motorcycle racing game you need to beat every computerized vehicle that you are " + ing + " against.")
+print("There are a whole lot of other cool games. When you play some games you win " + plural_noun + " for certain scores.")
+print("Once you're done you can cash in your tickets to get a big " + noun + ".")
+print("You can save your " + plural_noun + " for another time.")
+print("When I went to this arcade I didn't believe how much fun it would be. You might annoy your parents by asking them over and over if you can go back to there. So far I have had a lot of fun every time I've been to this great arcade!")
+
+## Mad Lib 8
+
+name = input("Enter your name: ")
+date = input("Enter a date: ")
+last_name = input("Enter a last name: ")
+noun = input("Enter a noun: ")
+letter = input("Enter a letter: ")
+noun = input("Enter a 3 letter noun: ")
+letter = input("Enter another letter: ")
+noun = input("Enter another 3 letter noun: ")
+noun = input("Enter a noun: ")
+plural_noun = input("Enter a plural noun: ")
+letter = input("Enter another letter: ")
+noun = input("Enter another 3 letter noun: ")
+letter = input("Enter another letter: ")
+noun = input("Enter another 3 letter noun: ")
+sound = input("Enter a sound: ")
+sound = input("Enter a sound: ")
+sound = input("Enter a sound: ")
+sound = input("Enter a sound: ")
+sound = input("Enter a sound: ")
+sound = input("Enter a sound: ")
+letter = input("Enter another letter: ")
+noun = input("Enter another 3 letter noun: ")
+letter = input("Enter another letter: ")
+noun = input("Enter another 3 letter noun: ")
+
+print("Name: " + name )
+print("Date: " + date )
+
+print(         "Big Mac Who?")
+print("Big Mc " + last_name + " had a " + noun + ",")
+print(letter + "- " + noun + ", " + letter + "- " + noun + " O.")
+
+print("On this " + noun + " he had some " + plural_noun + ", " + letter + " -3 " + noun + ", " + letter + "- " + noun + " O.")
+
+print("With a " + sound + ", " + sound + " here, and a " + sound + " " + sound + " there, everywhere a " + sound + "- " + sound + " " + letter + "- " + noun + ", " + letter + "- " + noun + " O.")
+
+## Mad Lib 9
+
+object = input("Enter an object: ")
+adjective = input("Enter an adjective: ")
+adjective = input("Enter another adjective: ")
+place = input("Enter a place: ")
+hobby = input("Enter a hobby: ")
+president = input("Enter the name of a president: ")
+food = input("Enter your fav food: ")
+
+print("Send up a signal throw me a/an " + object )
+print("It might not be " + adjective + " but it sure ain't " + adjective )
+print("I'm one step from " + place + " and two steps behind")
+print("I picked up " + hobby + " I called " + president + " who said " + food + " was the new national meal")
+
+## Mad Lib 10
+
+print("         Life In Technicolor")
+
+adjective = input("Enter an adjective: ")
+verb = input("Enter a verb in ing form: ")
+thing = input("Enter an interesting happening: ")
+celebrity = input("Enter a celebrity: ")
+adjective = input("Enter an adjective: ")
+
+print("There's a " + adjective + " wind blowing")
+print("Every night there, the headlights are " + verb )
+print("There's a " + thing + " coming")
+print("On the radio I heard " + celebrity + " talking about their pet's addiction to snickers")
+print("Baby, it's a " + adjective + " world")
