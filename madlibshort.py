5435ddcf20c5adb01aaa9b80abb3846fa082375e
root <root@instance-2.us-east4-c.c.mad-libs-221518.internal>
2018-11-11 22:17:28 +0000

    shortened version of mad libs

---
+++ madlibshort.py
@@ -0,0 +1,103 @@
+words = ['funny_thing', 'place', 'color', 'plural_noun', 'celebrity', 'date', 'full_name', 'a_place', 'subject', 'signature', 'noun', 'plant', 'hobby', 'object', 'food', 'funny_things', 'verb', 'adjective', 'imaginary_friend', 'game', 'body_part', 'verb_ing', 'number', 'adverb', 'verb_past', 'animal', 'fruit', 'last_name', 'group', 'president', 'thing']
+a = {}
+
+for word in words:
+    choose = input(f"Enter a(n) {word}: ")
+    a[word] = choose
+
+print(f"Roses are {a['color']}")
+print(f"{a['plural_noun']} are blue")
+print(f"I love {a['plural_noun']}")
+
+## Mad Lib 1
+
+print(f"Date: {a['date']}")
+print(f"{a['full_name']} is authorized to be at {a['a_place']}")
+print(f"instead of {a['subject']} class.")
+print(f"SIGNED: {a['signature']}")
+
+## Mad Lib 2
+
+print("                  We Are The Champions") 
+print(f"I've taken my {a['noun']} out.")
+print(f"And my {a['object']} calls")
+print(f"You brought me {a['food']} and {a['funny_things']} and everything that comes with it")
+print(f"I {a['verb']} you all.")
+
+## Mad Lib 3
+
+print("          Under The Bridge")
+print(f"Sometimes I feel like {a['adjective']}")
+print(f"I don't have a {a['object']}")
+print(f"Sometimes I feel like my only friend is {a['imaginary_friend']}")
+print(f"The {a['adjective']} city ")
+print(f"Lonely as I am, together we {a['verb']}")
+
+## Mad Lib 4
+
+print(f"A vacation is when you take a trip to some {a['adjective']} place with your {a['adjective']} family. Usually you go to some place that is near a/an {a['noun']}, or up on a/an {a['noun']}")
+print(f"A good vacation place is one where you can ride {a['plural_noun']}, or play {a['game']} or go hunting for {a['plural_noun']}")
+print(f"I like to spend my time {a['verb_ing']} {a['verb_ing']}. When parents go on a vacation, they spend their time eating three {a['plural_noun']} a day, and fathers play golf, and mothers sit around {a['verb_ing']}")
+print(f"Last summer, my little brother fell in a/an {a['noun']} and got poison {a['plant']} all over his {a['body_part']}")
+print(f"My family is going to go to (the) {a['place']}, and I will practice {a['verb_ing']}.")
+print(f"Parents need vacations more than kids because parents are always very {a['adjective']} and because they have to work {a['number']} hours every day all year making enough {a['plural_noun']} to pay for the vacation.")
+
+## Mad Lib 5
+
+print(f"Name: {a['full_name']}      Date:  {a['date']}")
+print(f"Today I went to the zoo. I saw a {a['adjective']} {a['animal']} jumping up and down in its tree.")
+print(f"He {a['verb_past']} {a['adverb']} through the large tunnel that led to its {a['adjective']} {a['noun']}")
+print(f"I got some peanuts and passed them through the cage to a gigantic gray {a['animal']} towering above my head.")
+print(f"Feeding the animals made me hungry. I went to get a {a['adjective']} scoop of ice cream. It filled my stomach.")
+print(f"Afterwards I had to {a['verb']} {a['adverb']} to catch our bus.")
+print(f"When I got home I {a['verb_past']} my mom for a {a['adjective']} day at the zoo.")
+
+## Mad Lib 6
+
+print("              The Rose Family")
+print(f"The rose is a(n) {a['adjective']}.")
+print(f"And was always a(n) {a['adjective']}.")
+print(f"But the {a['noun']} now {a['verb']}")
+print(f"That the {a['fruit']} is a {a['noun']}")
+print(f"And the {a['fruit']} is, and so is ")
+print(f"The {a['object']}, I suppose.")
+print(f"The {a['animal']} only knows")
+print(f"What will next prove a rose {a['adjective']}")
+print(f"{a['plural_noun']}, of course, are a rose -")
+print(f"But were always a rose.")
+
+## Mad Lib 7
+
+print(f"Name: {a['full_name']}            Date: {a['date']}")
+print("          At The Arcade!")
+print(f"When I go to the arcade with my {a['plural_noun']} there are lots of games to play.")
+print(f"I spend lots of time there with my friends. In 'Xmen' you can be different {a['plural_noun']}. The point of the game is to {a['verb']} every robot.")
+print(f"You also need to save people, and then you can go to the next level. In 'Star Wars' you are {a['celebrity']} and you try to destroy every {a['noun']}")
+print(f"In a car racing / motorcycle racing game you need to beat every computerized vehicle that you are {a['verb_ing']} against.")
+print(f"There are a whole lot of other cool games. When you play some games you win {a['plural_noun']} for certain scores.")
+print(f"Once you're done you can cash in your tickets to get a big {a['noun']}. You can save your {a['plural_noun']} for another time.")
+print(f"When I went to this arcade I didn't believe how much fun it would be. You might annoy your parents by asking them over and over if you can go back to {a['a_place']}. So far I have had a lot of fun every time I've been to this great arcade!")
+
+## Mad Lib 8
+
+print(f"Name: {a['full_name']}       Date: {a['date']}")
+print(         "Big Mac Who?")
+print(f"Big Mc {a['last_name']} had a {a['noun']}. A group of {a['group']} ordered a Mc {a['last_name']}, but received a(n) {a['animal']} instead.")
+print(f"The group decided to call {a['celebrity']} to report the issue, but {a['celebrity']} answered the phone. The group was so {a['verb_past']} they forgot to make the claim. They then got pulled over by {a['funny_thing']} claiming to be the police.")
+print(f"One group member was so scared that could not {a['verb']} anymore.")
+
+## Mad Lib 9
+
+print(f"Send up a signal, throw me a/an {a['object']}. It might not be {a['adjective']}, but it sure ain't {a['adjective']}")
+print(f"I'm one step from {a['a_place']} and two steps behind")
+print(f"I picked up {a['hobby']}. I called {a['president']}, who agreed {a['food']} was the new fav meal nationwide")
+
+## Mad Lib 10
+
+print("         Life In Technicolor")
+
+print(f"There's a(n) {a['adjective']} wind blowing")
+print(f"Every night there, the headlights are {a['verb_ing']}")
+print(f"There's a {a['thing']} coming")
+print(f"On the radio I heard {a['celebrity']} talking about their pet's addiction to {a['food']}")
+print(f"Baby, it's a(n) {a['adjective']} world")
